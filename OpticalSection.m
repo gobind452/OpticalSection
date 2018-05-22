cd8cb480fe0a9bd88a58e83f298f30ecf42cebfa
gobind452 <30503480+gobind452@users.noreply.github.com>
2018-05-23 02:01:07 +0530

    Update OpticalSection.m

--- OpticalSection.m
+++ OpticalSection.m
@@ -8,8 +8,8 @@ grating3 = double(imread("grating3.jpg"));
 grating1 = grating1/max(max(grating1));
 grating2 = grating2/max(max(grating2));
 grating3 = grating3/max(max(grating3));
-image1 = DefocusImage(image,z,grating1,1);
+image1 = DefocusImage(image,z,grating1,1); %Running defocusing on the multiplied images
 image2 = DefocusImage(image,z,grating2,1);
 image3 = DefocusImage(image,z,grating3,1);
 section = sqrt((image2-image1).*(image2-image1) + (image3-image2).*(image3-image2) + (image3-image1).*(image3-image1))*sqrt(2)/3;
-end
+end % final reconstruction
